3a960f2c329f305fc1faf1ad2ffe64a3a44c78dd
adewale-ht <seunade14@gmail.com>
2025-09-06 23:11:12 +0100

    starting a new course

---
+++ js/date.js
@@ -0,0 +1,6 @@
+// date.js
+// Copyright year
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('currentyear').textContent = new Date().getFullYear();
+  document.getElementById('lastModified').textContent = 'Last Modified: ' + document.lastModified;
+});
